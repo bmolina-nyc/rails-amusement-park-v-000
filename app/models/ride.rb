5fe73c8e671b3f6d9cf54a553be74421a943b2ec
bmolina-nyc <bruce.d.molina@gmail.com>
2016-12-13 20:01:51 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,22 @@
 class Ride < ActiveRecord::Base
   # write associations here
+  belongs_to :attraction
+  belongs_to :user
+
+
+  # you don't need to say self here at all but it helps you to read it 
+  def take_ride
+    if self.user.tickets < self.attraction.tickets && self.attraction.min_height > self.user.height 
+      "Sorry. You do not have enough tickets the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif self.user.tickets < self.attraction.tickets
+      "Sorry. You do not have enough tickets the #{attraction.name}."
+    elsif self.attraction.min_height > self.user.height 
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      self.user.tickets = (self.user.tickets -  self.attraction.tickets)
+      self.user.nausea = (self.user.nausea + self.attraction.nausea_rating)
+      self.user.happiness = (self.user.happiness + self.attraction.happiness_rating)
+      user.save
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  resources :sessions, only:[:new, :show, :create]
+
+  get '/signin' => 'sessions#new'
+
+  root 'static#root'
+
+  post "/logout" => "sessions#destroy", as: :logout
+
+  resources :users
+
 
 
 end
