d2fb32fbfadc97aceb9c9e67ba7815097769c73c
bmolina-nyc <bruce.d.molina@gmail.com>
2016-12-14 22:06:42 -0500

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -4,17 +4,40 @@ class AttractionsController < ApplicationController
       @attractions = Attraction.all
   end
 
+  def new 
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+
+    if @attraction.save 
+      flash[:notice] = "#{@attraction.name} created!"
+      redirect_to attraction_path(@attraction)
+    else
+      flash[:notice] = "Sorry - couldn't create"
+      redirect_to new_attraction_path
+    end
+  end
+
   def show
     @user = current_user
     @attraction = Attraction.find_by(id: params[:id])
   end
 
-  def update 
-    @user = current_user
+  def edit
     @attraction = Attraction.find_by(id: params[:id])
-  
+  end
 
-    if @user.height > @attraction.min_height && @user.tickets > @attraction.tickets
+  def update 
+    @attraction = Attraction.find_by(id: params[:id])
+    @user = current_user
+
+    if params[:user_id].nil? && current_user.admin?
+      @attraction.update(attraction_params)
+       flash[:notice] = "#{@attraction.name} has been updated!"
+      redirect_to attraction_path(@attraction)
+    elsif @user.height > @attraction.min_height && @user.tickets > @attraction.tickets
       flash[:notice] = "Thanks for riding the #{@attraction.name}!"
         @user.tickets -= @attraction.tickets
         @user.nausea += @attraction.nausea_rating
@@ -30,6 +53,10 @@ class AttractionsController < ApplicationController
         flash[:notice] = "You do not have enough tickets to ride the #{@attraction.name}"
         redirect_to user_path(@user)
     end
+  end
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
   end
 
 end
